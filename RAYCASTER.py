78fe4f47421868a8d7e030099315ba40b0dcd917
DRR <drr@MacBook-Air-de-Axis.local>
2022-09-08 21:56:08 +0200

    Working collision detection

--- RAYCASTER.py
+++ RAYCASTER.py
@@ -9,6 +9,7 @@ from pimoroni import Button
 display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, pen_type=PEN_P8)
 display.set_backlight(1.0)
 
+# 240X135
 HEIGHT, WIDTH = display.get_bounds() # flipped as we use it horizontally
 
 button_a = Button(12)
@@ -18,63 +19,24 @@ button_y = Button(15)
 
 BLACK = display.create_pen(0,0,0)
 WHITE = display.create_pen(255,255,255)
+GREEN = display.create_pen(0,255,0)
+RED = display.create_pen(255,0,0)
+BLOCK_SIZE = 10
+LEVEL = """
+########################
+#      ###  #     #    #
+####    ##    #####    #
+#                      #
+#                      #
+#                      #
+#                      #
+#                      #
+#                      #
+#                      #
+#                      #
+########################"""
 
 clamp = lambda x,min,max: min if x < min else (max if x > max else x)
-'''
-COS_X = {
-    0:1.0,
-    15:0.97,
-    30:0.87,
-    45:0.71,
-    60:0.5,
-    75:0.26,
-    90:0.0,
-    105:-0.26,
-    120:-0.5,
-    135:-0.71,
-    150:-0.87,
-    165:-0.97,
-    180:-1.0,
-    195:-0.97,
-    210:-0.87,
-    225:-0.71,
-    240:-0.5,
-    255:-0.26,
-    270:-0.0,
-    285:0.26,
-    300:0.5,
-    315:0.71,
-    330:0.87,
-    345:0.97
-}
-
-SIN_X = {
-    0:0.0,
-    15:0.26,
-    30:0.5,
-    45:0.71,
-    60:0.87,
-    75:0.97,
-    90:1.0,
-    105:0.97,
-    120:0.87,
-    135:0.71,
-    150:0.5,
-    165:0.26,
-    180:0.0,
-    195:-0.26,
-    210:-0.5,
-    225:-0.71,
-    240:-0.87,
-    255:-0.97,
-    270:-1.0,
-    285:-0.97,
-    300:-0.87,
-    315:-0.71,
-    330:-0.5,
-    345:-0.26
-}
-'''
 # this function takes the screen as follows:
 # 0,0 ____________________________
 #  A |                            | X
@@ -90,18 +52,6 @@ def draw_vline(x, y1, y2, thickness):
 def modulus(x):
     if x < 0: return x*-1
     return x
-# this function uses the values in COS_X table
-# and finds the closest match
-'''def acos(x):
-    # print(x)
-    min_dist = 1000
-    winner = None
-    for key,val in COS_X.items():
-        d = modulus(x-val)
-        if d < min_dist:
-            min_dist = d
-            winner = key
-    return winner'''
     
 # rotate a vertex by an angle upon a point
 # as we use the point to find the radius,
@@ -132,7 +82,7 @@ def rotate(v,a,p,r=None):
 # draw a rectangle with left-top corner at x,y with
 # width w and height h, rotated by angle a
 # note: rotated rects (squares) are not filled
-def draw_rect(x,y,w,h,a=0):
+def draw_rect(x,y,w,h,a=0,draw=True):
     overtices = [(x,y),(x+w,y),(x+w,y+h),(x,y+h)]
     center = (x+w/2, y + w/2)
     # MicroPython does not include list comprehension
@@ -146,6 +96,8 @@ def draw_rect(x,y,w,h,a=0):
     #for v in vertices:
      #   display.character(c,int(v[1]),int(v[0]),1)
       #  c+=1
+    if not draw:
+        return vertices
     for i in range(0,len(vertices)):
         if i: l = i-1
         else: l = len(vertices)-1
@@ -154,8 +106,19 @@ def draw_rect(x,y,w,h,a=0):
             int(vertices[i][0]),
             int(vertices[l][1]),
             int(vertices[l][0])
-            )
+        )
     return vertices
+def colliding(v,x,y):
+    # bounds = [x+BLOCK_SIZE/2,x-BLOCK_SIZE/2,y+BLOCK_SIZE/2,y-BLOCK_SIZE/2]
+    bounds = [x,y,x+BLOCK_SIZE,y+BLOCK_SIZE]
+    display.set_pen(GREEN)
+    display.rectangle(y,x,BLOCK_SIZE,BLOCK_SIZE)
+    for s in v:
+        display.rectangle(int(s[1]),int(s[0]),1,1)
+        display.set_pen(WHITE)
+        if s[0] > bounds[0] and s[0] < bounds[2] and s[1] > bounds[1] and s[1] < bounds[3]:
+            return True
+    return False
 class Player:
     def __init__(self,x,y,sz):
         self.x = x
@@ -163,7 +126,6 @@ class Player:
         self.size = sz
         self.a = 0
         self.hsz = round(self.size/2)
-        # self.vertices = [(x,y),(x+w,y),(x+w,y+h),(x,y+h)]
         # precomputing radius to not waste power
         # note that, as it is a square with equal side lengths,
         # we can take the original
@@ -174,8 +136,32 @@ class Player:
     def move(self,power):
         # farg is arbitrary as it is used as ref point
         nx,ny = rotate((self.x + power,self.y),self.a,(self.x,self.y),power)
+        oldx,oldy = self.x,self.y
         self.x = nx
         self.y = ny
+        # collision detection, one step forward
+        # overtices = [(self.x,self.y),(self.x+self.size,self.y),(self.x+self.size,self.y+self.size),(self.x,self.y+self.size)]
+        # vertices = []
+        # for v in overtices:
+          #  vertices.append(rotate(v,self.a,(self.x+self.hsz,self.y+self.hsz)))
+        # if not self.a:
+         #   vertices = overtices
+        vertices = draw_rect(self.x - self.hsz, self.y - self.hsz,self.size,self.size,self.a,False)
+        ok = True
+        for i,line in enumerate(LEVEL.split('\n')):
+            for j,char in enumerate(line):
+                bx,by = j*BLOCK_SIZE,i*BLOCK_SIZE
+                if char == "#" and colliding(vertices,bx,by):
+                    ok = False
+                    break
+            if not ok: break
+        if not ok:
+            print("COLLISION!")
+            # mod = (2 * (power < 0)) - 1
+            # nx,ny = rotate((self.x + mod*5,self.y),self.a,(self.x,self.y),mod*5)
+            self.x = oldx
+            self.y = oldy
+            return
     def rotate(self,ang):
         self.a += ang
     def draw(self):
@@ -183,7 +169,9 @@ class Player:
         # farg is arbitrary as it is used as ref point
         lx,ly = rotate((self.x + self.size*1.2,self.y),self.a,(self.x,self.y),self.size*1.2)
         display.line(round(self.y),round(self.x),round(ly),round(lx))
-player = Player(50,50,10)
+#class Block:
+ #   def __init__(self,x,y,sz):
+player = Player(50,50,BLOCK_SIZE)
 while True:
     display.set_pen(BLACK)
     display.clear()
@@ -204,5 +192,10 @@ while True:
         player.rotate(-5)
     if button_y.read():
         player.rotate(5)
+        
+    for i,line in enumerate(LEVEL.split('\n')):
+        for j,char in enumerate(line):
+            if char == "#":
+                display.rectangle(i*BLOCK_SIZE ,j*BLOCK_SIZE,9,9)
     display.update()
     time.sleep(0.01)
